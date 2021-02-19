08af6ca76c02d7f4941a7d2b59fba302650fe0a4
ogoro <mi.ogoro@gmail.com>
2021-02-19 16:09:13 +0200

    bats_bunker - initial commit

---
+++ bats_bunker.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def checkio(bunker: List[str]) -> [int, float]:
+    return 2.83
+
+
+if __name__ == '__main__':
+    def almost_equal(checked, correct, significant_digits=2):
+        precision = 0.1 ** significant_digits
+        return correct - precision < checked < correct + precision
+
+    assert almost_equal(checkio([
+        "B--",
+        "---",
+        "--A"]), 2.83), "1st example"
+    # assert almost_equal(checkio([
+    #     "B-B",
+    #     "BW-",
+    #     "-BA"]), 4), "2nd example"
+    # assert almost_equal(checkio([
+    #     "BWB--B",
+    #     "-W-WW-",
+    #     "B-BWAB"]), 12), "3rd example"
+    # assert almost_equal(checkio([
+    #     "B---B-",
+    #     "-WWW-B",
+    #     "-WA--B",
+    #     "-W-B--",
+    #     "-WWW-B",
+    #     "B-BWB-"]), 9.24), "4th example"
