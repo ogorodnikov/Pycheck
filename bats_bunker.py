3d855b16229e538cbc1251d03de67174cf9b6e6a
ogoro <mi.ogoro@gmail.com>
2021-02-20 02:38:00 +0200

    bats_bunker - bat_connections

--- bats_bunker.py
+++ bats_bunker.py
@@ -7,6 +7,7 @@ BAT = 'B'
 ALPHA_BAT = 'A'
 WALL = 'W'
 EMPTY = '-'
+CAVE_ENTRANCE = (0 + 0j)
 
 
 def map_to_field(bunker_map):
@@ -22,53 +23,109 @@ def check_connection(bat_a, bat_b, wall):
     print('Bat b:', bat_b)
 
     wall_corners = [wall + complex(*delta) for delta in product((-0.5, 0.5), repeat=2)]
-    print('Wall corners:', wall_corners)
+    print('    Wall corners:', wall_corners)
 
     bat_a_to_corners_vectors = [wall_corner - bat_a for wall_corner in wall_corners]
-    print('Bat a to wall corners vectors:', bat_a_to_corners_vectors)
+    print('    Bat a to wall corners vectors:', bat_a_to_corners_vectors)
 
     angles = [phase(vector) for vector in bat_a_to_corners_vectors]
-    print('Angles:', angles)
+    print('    Angles:', angles)
 
     min_angle = min(angles)
     max_angle = max(angles)
-    print('Min angle:', min_angle)
-    print('Max angle:', max_angle)
+    print('    Min angle:', min_angle)
+    print('    Max angle:', max_angle)
 
     bat_a_to_b_angle = phase(bat_b - bat_a)
-    print('Bat a to b angle:', bat_a_to_b_angle)
+    print('    Bat a to b angle:', bat_a_to_b_angle)
 
-    is_connected = not(min_angle <= bat_a_to_b_angle <= max_angle)
+    bat_a_to_wall_distance = abs(wall - bat_a)
+    print('    Bat a to wall distance:', bat_a_to_wall_distance)
+
+    bat_a_to_bat_b_distance = abs(bat_b - bat_a)
+    print('    Bat a to bat b distance:', bat_a_to_bat_b_distance)
+
+    wall_is_closer_then_bat_b = bat_a_to_wall_distance < bat_a_to_bat_b_distance
+    print('    Wall is closer then bat b:', wall_is_closer_then_bat_b)
+
+    is_in_sector = min_angle <= bat_a_to_b_angle <= max_angle
+    print('    Is in sector:', is_in_sector)
+    is_connected = not (is_in_sector and wall_is_closer_then_bat_b)
+
     print('Is connected:', is_connected)
     print()
     return is_connected
 
 
+def find_shortest_path(start, goal, connections):
+    print('Start:', start)
+
+    if start == goal:
+        return 0
+
+    distances = []
+    for neighbour in connections[start]:
+        print('    Neighbour:', neighbour)
+
+        distance = abs(neighbour - start)
+        print('        Distance:', distance)
+
+        if neighbour == goal:
+            print('        === Goal found, returning distance:', distance)
+            return distance
+
+        new_connections = {key: {e for e in value} for key, value in connections.items()}
+        new_connections[start] -= {neighbour}
+        new_connections[neighbour] -= {start}
+        print('        New connections:', new_connections)
+
+        neighbour_to_goal_distance = find_shortest_path(neighbour, goal, new_connections)
+        print('        Neighbour to goal distance:', neighbour_to_goal_distance)
+
+        total_distance = distance + neighbour_to_goal_distance
+        print('        Total distance:', total_distance)
+
+        distances.append(total_distance)
+
+    print('Distances from', start, ':', distances)
+
+    if not distances:
+        return float('inf')
+
+    min_distance = min(distances)
+    print('Min distance from', start, ':', min_distance)
+    print()
+    return min_distance
+
+
 def checkio(bunker: List[str]) -> [int, float]:
-    print('Bunker:', bunker)
+    print('Bunker:')
+    [print(row) for row in bunker]
 
     field = map_to_field(bunker)
     print('Field:', field)
 
-    # bat_2 = field[BAT].pop()
-    # bat_1 = field[BAT].pop()
-    # wall = field[WALL].pop()
-    # print('Bat 1:', bat_1)
-    # print('Bat 2:', bat_2)
-    # print('Wall:', wall)
-    #
-    # check_connection(bat_1, bat_2, wall)
+    all_bats = field[BAT] + field[ALPHA_BAT]
+    print('All bats:', all_bats)
+    print()
 
-    for bat_a, bat_b in permutations(field[BAT], 2):
-        for wall in field[WALL]:
-            check_connection(bat_a, bat_b, wall)
+    bat_connections = defaultdict(set)
+    for bat_a, bat_b in permutations(all_bats, 2):
+        if all(check_connection(bat_a, bat_b, wall) for wall in field[WALL]):
+            print(bat_a, 'is connected with', bat_b)
+            bat_connections[bat_a] |= {bat_b}
+        else:
+            print(bat_a, 'is not connected with', bat_b)
 
+    print('Bat connections:')
+    for bat, connections in bat_connections.items():
+        print(f'{bat:7}: {connections}')
+    print()
 
-
-
-
-
-    return 4
+    shortest_path = find_shortest_path(CAVE_ENTRANCE, field[ALPHA_BAT].copy().pop(), bat_connections)
+    print('Shortest path:', shortest_path)
+    print()
+    return shortest_path
 
 
 if __name__ == '__main__':
@@ -81,16 +138,22 @@ if __name__ == '__main__':
     #     "B--",
     #     "---",
     #     "--A"]), 2.83), "1st example"
-
-    assert almost_equal(checkio([
-        "B-B",
-        "BW-",
-        "-BA"]), 4), "2nd example"
+    #
+    # assert almost_equal(checkio([
+    #     "B-B",
+    #     "BW-",
+    #     "-BA"]), 4), "2nd example"
 
     # assert almost_equal(checkio([
-    #     "BWB--B",
-    #     "-W-WW-",
-    #     "B-BWAB"]), 12), "3rd example"
+    #     "B--",
+    #     "-BA",
+    #     "--W"]), 2.24), "2nd example"
+
+    assert almost_equal(checkio([
+        "BWB--B",
+        "-W-WW-",
+        "B-BWAB"]), 12), "3rd example"
+
     # assert almost_equal(checkio([
     #     "B---B-",
     #     "-WWW-B",
