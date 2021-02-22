5b0c2a37dd6311e1c8c62c56f0abfac06660b638
ogoro <mi.ogoro@gmail.com>
2021-02-22 20:30:30 +0200

    behind_2048 - shift_line

--- behind_2048.py
+++ behind_2048.py
@@ -1,8 +1,40 @@
+from itertools import tee
+
+FIELD_WIDTH = 4
+
+
+def shift_line(line):
+    print('Line:', line)
+
+    filtered_line = list(filter(None, line))
+    print('Filtered line:', filtered_line)
+
+    a_iter, b_iter = tee(filtered_line)
+    next(b_iter, None)
+
+    shifted_line = []
+    for a, b in zip(a_iter, b_iter):
+        print('A:', a)
+        print('B:', b)
+
+        if a == b:
+            shifted_line.append(a + b)
+        else:
+            shifted_line.append(a)
+
+    shifted_line += [0] * (FIELD_WIDTH - len(shifted_line))
+
+    print('Shifted line:', shifted_line)
+    print()
+    return shifted_line
+
+
 def move2048(state, move):
     print('State:')
     [print(row) for row in state]
     print()
 
+    lines = []
     if move == 'left':
         lines = state
     elif move == 'up':
@@ -14,6 +46,23 @@ def move2048(state, move):
 
     print('Lines:')
     [print(line) for line in lines]
+
+    shifted_lines = [shift_line(line) for line in lines]
+    print('Shifted lines:')
+    [print(line) for line in shifted_lines]
+
+    shifted_field = []
+    if move == 'left':
+        shifted_field = shifted_lines
+    elif move == 'up':
+        shifted_field = [line for line in zip(*shifted_lines)]
+    elif move == 'right':
+        shifted_field = [line[::-1] for line in shifted_lines]
+    elif move == 'down':
+        shifted_field = [line[::-1] for line in zip(*shifted_lines)]
+
+    print('Shifted field:')
+    [print(line) for line in shifted_field]
 
     return state
 
