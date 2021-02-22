cadf7f9459bc0d997d008a2ea367a69423385959
ogoro <mi.ogoro@gmail.com>
2021-02-22 02:33:54 +0200

    eaten_go_stones - get_liberties

--- eaten_go_stones.py
+++ eaten_go_stones.py
@@ -16,33 +16,62 @@ def board_to_field(board):
 
 def get_stone_groups(field):
 
+    new_field = {key: set(value) for key, value in field.items()}
+
     groups = defaultdict(list)
 
     def get_group(color):
         group = set()
-        q = [field[color].pop()]
+        q = [new_field[color].pop()]
         while q:
             a = q.pop()
             group |= {a}
             for b in (a + delta for delta in DELTAS):
-                if b in field[color]:
-                    field[color] -= {b}
+                if b in new_field[color]:
+                    new_field[color] -= {b}
                     group |= {b}
                     q.append(b)
         groups[color] += [group]
 
     while True:
-        if field[WHITE]:
+        if new_field[WHITE]:
             get_group(WHITE)
-        elif field[BLACK]:
+        elif new_field[BLACK]:
             get_group(BLACK)
         else:
             return groups
 
 
+def get_liberties(stone_group):
+    liberties = set()
+    for stone in stone_group:
+        liberties |= {stone + delta for delta in DELTAS}
+    liberties -= stone_group
+    return liberties
+
 
 def get_eaten_counter(stone_groups, field):
-    pass
+    print('Field:', field)
+    all_cells = {cell for key in field for cell in field[key]}
+    print('All cells:', all_cells)
+    print('Len all cells :', len(all_cells))
+
+    for color in stone_groups:
+        print('Color:', color)
+        if color == WHITE:
+            opposite_color = BLACK
+        else:
+            opposite_color = WHITE
+
+        for stone_group in stone_groups[color]:
+            print('Stone group:', stone_group)
+
+            liberties = get_liberties(stone_group) & all_cells
+            print('    Liberties:', liberties)
+
+            is_group_surrounded = liberties <= field[opposite_color]
+            print('    Is group surrounded:', is_group_surrounded)
+
 
 
 def go_game(board):
