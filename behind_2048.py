d07ea9aecc9eb74eda6853bc1745731ed49e528f
ogoro <mi.ogoro@gmail.com>
2021-02-23 02:30:01 +0200

    behind_2048 - victory

--- behind_2048.py
+++ behind_2048.py
@@ -4,6 +4,9 @@ FIELD_WIDTH = 4
 NEW_TILE_PRIORITIES = list(reversed(range(FIELD_WIDTH ** 2)))
 NEW_TILE = 2
 EMPTY = 0
+GOAL = 2048
+VICTORY_BANNER = 'UWIN'
+LOSE_BANNER = 'GAMEOVER'
 
 
 def shift_line(line):
@@ -93,35 +96,39 @@ def move2048(state, move):
     print('Shifted field with new 2:')
     [print(line) for line in shifted_field]
     print()
+
+    if any(cell == GOAL for row in shifted_field for cell in row):
+        print('Goal reached')
+        victory_field = [list(VICTORY_BANNER)] * FIELD_WIDTH
+        print('Victory field:', victory_field)
+        return victory_field
+
     return shifted_field
 
 
 if __name__ == '__main__':
 
-    assert move2048([[0, 2, 0, 0],
-                     [0, 0, 0, 0],
-                     [0, 0, 0, 0],
-                     [0, 2, 0, 0]], 'up') == [[0, 4, 0, 0],
-                                              [0, 0, 0, 0],
-                                              [0, 0, 0, 0],
-                                              [0, 0, 0, 2]], "Start. Move Up!"
-
-    assert move2048([[4, 0, 0, 0],
-                     [0, 4, 0, 0],
-                     [0, 0, 0, 0],
-                     [0, 0, 8, 8]], 'right') == [[0, 0, 0, 4],
-                                                 [0, 0, 0, 4],
-                                                 [0, 0, 0, 0],
-                                                 [0, 0, 2, 16]], "Simple right"
-
-    assert move2048([[2, 0, 2, 2],
-                     [0, 4, 4, 4],
-                     [8, 8, 8, 16],
-                     [0, 0, 0, 0]], 'right') == [[0, 0, 2, 4],
-                                                 [0, 0, 4, 8],
-                                                 [0, 8, 16, 16],
-                                                 [0, 0, 0, 2]], "Three merging"
-
+    # assert move2048([[0, 2, 0, 0],
+    #                  [0, 0, 0, 0],
+    #                  [0, 0, 0, 0],
+    #                  [0, 2, 0, 0]], 'up') == [[0, 4, 0, 0],
+    #                                           [0, 0, 0, 0],
+    #                                           [0, 0, 0, 0],
+    #                                           [0, 0, 0, 2]], "Start. Move Up!"
+    # assert move2048([[4, 0, 0, 0],
+    #                  [0, 4, 0, 0],
+    #                  [0, 0, 0, 0],
+    #                  [0, 0, 8, 8]], 'right') == [[0, 0, 0, 4],
+    #                                              [0, 0, 0, 4],
+    #                                              [0, 0, 0, 0],
+    #                                              [0, 0, 2, 16]], "Simple right"
+    # assert move2048([[2, 0, 2, 2],
+    #                  [0, 4, 4, 4],
+    #                  [8, 8, 8, 16],
+    #                  [0, 0, 0, 0]], 'right') == [[0, 0, 2, 4],
+    #                                              [0, 0, 4, 8],
+    #                                              [0, 8, 16, 16],
+    #                                              [0, 0, 0, 2]], "Three merging"
     # assert move2048([[256, 0, 256, 4],
     #                  [16, 8, 8, 0],
     #                  [32, 32, 32, 32],
@@ -129,13 +136,15 @@ if __name__ == '__main__':
     #                                              [0, 0, 16, 16],
     #                                              [0, 0, 64, 64],
     #                                              [0, 2, 8, 4]], "All right"
-    # assert move2048([[4, 4, 0, 0],
-    #                  [0, 4, 1024, 0],
-    #                  [0, 256, 0, 256],
-    #                  [0, 1024, 1024, 8]], 'down') == [['U', 'W', 'I', 'N'],
-    #                                                   ['U', 'W', 'I', 'N'],
-    #                                                   ['U', 'W', 'I', 'N'],
-    #                                                   ['U', 'W', 'I', 'N']], "We are the champions!"
+
+    assert move2048([[4, 4, 0, 0],
+                     [0, 4, 1024, 0],
+                     [0, 256, 0, 256],
+                     [0, 1024, 1024, 8]], 'down') == [['U', 'W', 'I', 'N'],
+                                                      ['U', 'W', 'I', 'N'],
+                                                      ['U', 'W', 'I', 'N'],
+                                                      ['U', 'W', 'I', 'N']], "We are the champions!"
+
     # assert move2048([[2, 4, 8, 16],
     #                  [32, 64, 128, 256],
     #                  [512, 1024, 2, 4],
