b1684fda80b50c11d83b35bd1150a5740cc4458c
ogoro <mi.ogoro@gmail.com>
2021-02-24 15:07:52 +0200

    the_territory_of_go - optimised

--- the_territory_of_go.py
+++ the_territory_of_go.py
@@ -32,39 +32,31 @@ def get_empty_cell_groups(field):
 
 
 def get_territory_counter(empty_cell_groups, field):
+
     all_cells = {cell for key in field for cell in field[key]}
     territory_counter = {BLACK: 0, WHITE: 0}
 
     for empty_cell_group in empty_cell_groups:
-        print('Empty cell group:', empty_cell_group)
 
         surrounding_cells = {cell + delta for delta in DELTAS for cell in
                              empty_cell_group} - empty_cell_group & all_cells
-        print('Surrounding cells:', surrounding_cells)
 
-        if surrounding_cells <= field[WHITE]:
-            print('Surrounded by white +', len(empty_cell_group))
-            territory_counter[WHITE] += len(empty_cell_group)
-        elif surrounding_cells <= field[BLACK]:
-            print('Surrounded by black +', len(empty_cell_group))
-            territory_counter[BLACK] += len(empty_cell_group)
+        for color in (WHITE, BLACK):
+            if surrounding_cells <= field[color]:
+                territory_counter[color] += len(empty_cell_group)
+                break
 
     return territory_counter
 
 
 def territory(board):
-    print('Board:')
-    [print(row) for row in board]
 
     field = board_to_field(board)
-    print('Field:', field)
 
     empty_cell_groups = get_empty_cell_groups(field)
-    print('Empty cell groups:', empty_cell_groups)
 
     territory_counter = get_territory_counter(empty_cell_groups, field)
-    print('Territory counter:', territory_counter)
-    print()
+
     return territory_counter
 
 
