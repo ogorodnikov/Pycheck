8270baada295330170a5b7999902b129f23ff99d
ogoro <mi.ogoro@gmail.com>
2021-02-25 04:09:08 +0200

    probably_dice - pass - optimised

--- probably_dice.py
+++ probably_dice.py
@@ -2,20 +2,14 @@ from itertools import combinations_with_replacement, permutations, groupby
 
 
 def probability(dice_number, sides, target):
-    print('Dice number:', dice_number)
-    print('Sides:      ', sides)
-    print('Target:     ', target)
-    print()
-
-    total_count = 0
 
     dice = list(range(1, sides + 1))
-    print('Dice:', dice)
+
+    print('Number of dices:', dice_number)
+    print('Target:         ', target)
+    print('Sides:          ', sides)
+    print('Dice:           ', dice)
     print()
-
-    total_permutations_count = sides ** dice_number
-
-    group_dict = {}
 
     dice_combinations = [combination for combination
                          in combinations_with_replacement(dice, dice_number)
@@ -24,32 +18,39 @@ def probability(dice_number, sides, target):
     combinations_count = len(dice_combinations)
     print('Combinations count:', combinations_count)
 
-    for i, combination in enumerate(dice_combinations):
-        print('    Combination:       ', list(combination))
-        print(f'    {i}/{combinations_count}: {i / combinations_count * 100:.2f}%')
+    group_dict = {}
 
-        groups = tuple(sorted(len(list(group[1])) for group in groupby(sorted(combination))))
-        print('    Groups:', groups)
+    total_fitting_permutations_count = 0
+    total_possible_permutations_count = sides ** dice_number
+
+    for i, combination in enumerate(dice_combinations):
+
+        combination_groups = groupby(sorted(combination))
+        group_lengths = (len(list(group[1])) for group in combination_groups)
+        groups = tuple(sorted(group_lengths))
 
         if groups in group_dict:
             permutations_count = group_dict[groups]
         else:
-            dice_permutations = set(permutations(combination, len(combination)))
+            dice_permutations = set(permutations(combination, dice_number))
             permutations_count = len(dice_permutations)
             group_dict[groups] = permutations_count
 
-        print('    Permutations count:', permutations_count)
+        total_fitting_permutations_count += permutations_count
 
-        total_count += permutations_count
-        print('    Total count:       ', total_count)
+        print(f'    {i}/{combinations_count}: {i / combinations_count * 100:.2f}%')
+        print('    Combination:       ', combination)
+        print('    Groups:            ', groups)
+        print('    Permutations count:', permutations_count)
+        print('    Total count:       ', total_fitting_permutations_count)
         print()
 
-    print('======================')
-    print('Total count:             ', total_count)
-    print('Total permutations count:', total_permutations_count)
+    target_probability = total_fitting_permutations_count / total_possible_permutations_count
 
-    target_probability = total_count / total_permutations_count
-    print('Target probability:      ', target_probability)
+    print('======================')
+    print('Total fitting permutations count: ', total_fitting_permutations_count)
+    print('Total possible permutations count:', total_possible_permutations_count)
+    print('Target probability:               ', target_probability)
     print()
     return target_probability
 
@@ -58,7 +59,6 @@ if __name__ == '__main__':
     def almost_equal(checked, correct, significant_digits=4):
         precision = 0.1 ** significant_digits
         return correct - precision < checked < correct + precision
-
 
     assert (almost_equal(probability(2, 6, 3), 0.0556)), "Basic example"
     assert (almost_equal(probability(2, 6, 4), 0.0833)), "More points"
