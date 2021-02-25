cdedfdd1a97376d1ab11f0e99ccda185227e53bb
ogoro <mi.ogoro@gmail.com>
2021-02-26 01:30:17 +0200

    warriors - fight - passed

--- Warriors/the_warriors.py
+++ Warriors/the_warriors.py
@@ -1,13 +1,29 @@
 class Warrior:
-    pass
+    def __init__(self):
+        self.health = 50
+        self.attack = 5
+
+    @property
+    def is_alive(self):
+        return self.health > 0
 
 
 class Knight(Warrior):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.attack = 7
+
+    @property
+    def is_alive(self):
+        return self.health > 0
 
 
 def fight(unit_1, unit_2):
-    return 0
+    while True:
+        for attacker, defender in (unit_1, unit_2), (unit_2, unit_1):
+            defender.health -= attacker.attack
+            if not defender.is_alive:
+                return defender == unit_2
 
 
 if __name__ == '__main__':
@@ -25,41 +41,3 @@ if __name__ == '__main__':
     assert dave.is_alive == False
     assert fight(carl, mark) == False
     assert carl.is_alive == False
-
-if False:
-    class Warrior:
-        def __init__(self):
-            self.hp = 50
-            self.attack = 5
-            print(self.hp, self.attack)
-
-        @property
-        def is_alive(self) -> bool:
-            return self.hp > 0
-
-
-    class Knight(Warrior):
-        def __init__(self):
-            super().__init__()
-            self.attack = 7
-            print(self.hp, self.attack)
-
-
-    def fight(unit_1, unit_2):
-        print(f"{unit_1.__class__.__name__}[{unit_1.hp, unit_1.attack}] and " +
-              f"{unit_2.__class__.__name__}[{unit_1.hp, unit_1.attack}] are fighting")
-
-        for i in range(100):
-            print("Round", i)
-            unit_2.hp -= unit_1.attack
-            print(unit_1.hp, ">", unit_2.hp)
-            if unit_2.hp > 0:
-                unit_1.hp -= unit_2.attack
-                print(unit_1.hp, "<", unit_2.hp)
-            if unit_2.hp <= 0:
-                print("Unit 1 won!")
-                return True
-            if unit_1.hp <= 0:
-                print("Unit 2 won!")
-                return False
-        return 0
