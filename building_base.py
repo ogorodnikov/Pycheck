f1d1d66636ffa61a9fa293c4e37cae2c979d0448
ogoro <mi.ogoro@gmail.com>
2021-03-06 00:20:44 +0200

    building_base - volume

--- building_base.py
+++ building_base.py
@@ -27,7 +27,7 @@ class Building:
         return self.width_WE * self.width_NS
 
     def volume(self):
-        raise NotImplementedError
+        return self.area() * self.height
 
     def __repr__(self):
         raise NotImplementedError
@@ -44,6 +44,6 @@ if __name__ == '__main__':
     assert json_dict(b.corners()) == {'north-east': [4, 4], 'south-east': [1, 4],
                                       'south-west': [1, 2], 'north-west': [4, 2]}, "Corners"
     assert b.area() == 6, "Area"
-    # assert b.volume() == 60, "Volume"
-    # assert b2.volume() == 30, "Volume2"
+    assert b.volume() == 60, "Volume"
+    assert b2.volume() == 30, "Volume2"
     # assert str(b) == "Building(1, 2, 2, 3, 10)", "String"
