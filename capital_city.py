9b7a78ea4e7f93f052fbb7aac62688521487e6d6
ogoro <mi.ogoro@gmail.com>
2021-03-06 18:30:37 +0200

    capital_city - solved

--- capital_city.py
+++ capital_city.py
@@ -1,9 +1,19 @@
 class Capital:
-    def __init__(self, city_name):
-        raise NotImplementedError
+    _instance = None
+    _city_name = None
+
+    def __new__(cls, city_name):
+        if cls._instance:
+            return cls._instance
+        cls._instance = super().__new__(cls)
+        cls._city_name = city_name
+        return cls._instance
+
+    def name(self):
+        return type(self)._city_name
+
 
 if __name__ == '__main__':
-
     ukraine_capital_1 = Capital("Kyiv")
     ukraine_capital_2 = Capital("London")
     ukraine_capital_3 = Capital("Marocco")
