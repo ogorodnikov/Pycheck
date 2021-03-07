8aaa5c7217e69f84a7d9d2e804bedacbd60aa21b
ogoro <mi.ogoro@gmail.com>
2021-03-07 02:16:07 +0200

    friends - remove

--- friends.py
+++ friends.py
@@ -5,13 +5,15 @@ class Friends:
         for connection in connections:
             self.add(connection)
 
-    def add(self, connection):
+    def add(self, connection: set) -> bool:
         is_new_connection = connection not in self._connections
         self._connections.add(frozenset(connection))
         return is_new_connection
 
-    def remove(self, connection):
-        raise NotImplementedError
+    def remove(self, connection: set) -> bool:
+        is_existing_connection = connection in self._connections
+        self._connections.discard(frozenset(connection))
+        return is_existing_connection
 
     def names(self):
         raise NotImplementedError
@@ -25,9 +27,9 @@ if __name__ == '__main__':
     digit_friends = Friends([{"1", "2"}, {"3", "1"}])
 
     assert letter_friends.add({"c", "d"}) is True, "Add"
-    # assert letter_friends.add({"c", "d"}) is False, "Add again"
-    # assert letter_friends.remove({"c", "d"}) is True, "Remove"
-    # assert digit_friends.remove({"c", "d"}) is False, "Remove non exists"
+    assert letter_friends.add({"c", "d"}) is False, "Add again"
+    assert letter_friends.remove({"c", "d"}) is True, "Remove"
+    assert digit_friends.remove({"c", "d"}) is False, "Remove non exists"
     # assert letter_friends.names() == {"a", "b", "c"}, "Names"
     # assert letter_friends.connected("d") == set(), "Non connected name"
     # assert letter_friends.connected("a") == {"b", "c"}, "Connected name"
