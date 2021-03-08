b89c5bccb5f62cad56021845ac1b254403fd35d0
ogoro <mi.ogoro@gmail.com>
2021-03-09 00:11:43 +0200

    hacker_language - new delete

--- hacker_language.py
+++ hacker_language.py
@@ -1,5 +1,6 @@
 import re
 
+
 class HackerLanguage:
     _message = ''
 
@@ -13,15 +14,16 @@ class HackerLanguage:
     #
     # and special signs ('.', ':', '!', '?', '@', '$', '%') won't be converted.
 
-    def scan(self, message):
+    @staticmethod
+    def encode(message):
 
-        tokens = []
-
-        def write_ascii_code(_, token):
-            return f'{ord(token):<07b}'
+        print('Encode message:', message)
 
         def write_transparently(_, token):
             return token
+
+        def write_ascii_code(_, token):
+            return f'{ord(token):<07b}'
 
         scanner = re.Scanner([(r'\d|[^\w\s]', write_transparently),
                               (r'\w|\s', write_ascii_code)])
@@ -31,19 +33,21 @@ class HackerLanguage:
         print('Tokens:', tokens)
         print('Unrecognised:', unrecognised)
 
-    @staticmethod
-    def encode(message):
-        print('Encode message:', message)
+        return ''.join(tokens)
 
-        encoded_message = ''
-        for symbol in message:
-            if symbol == ' ':
-                binary = WHITESPACE_BINARY
-            else:
-                binary = f'{ord(symbol):b}'
-            print('Binary:', binary)
-            encoded_message += binary
-        return encoded_message
+    # @staticmethod
+    # def encode(message):
+    #     print('Encode message:', message)
+    #
+    #     encoded_message = ''
+    #     for symbol in message:
+    #         if symbol == ' ':
+    #             binary = WHITESPACE_BINARY
+    #         else:
+    #             binary = f'{ord(symbol):b}'
+    #         print('Binary:', binary)
+    #         encoded_message += binary
+    #     return encoded_message
 
     @staticmethod
     def decode(message):
@@ -53,7 +57,13 @@ class HackerLanguage:
         self._message += text
 
     def delete(self, symbol_count):
-        self._message = self._message[:len(self._message) - symbol_count]
+        print('Before:')
+        print('Self message:', self._message)
+
+        self._message = self._message[:-symbol_count]
+
+        print('After:')
+        print('Self message:', self._message)
 
     def send(self):
         return self.encode(self._message)
@@ -64,12 +74,11 @@ class HackerLanguage:
 
 if __name__ == '__main__':
     message_1 = HackerLanguage()
-    message_1.scan('te st123 !@# 12:0')
 
-    # message_1.write("secrit")
-    # message_1.delete(2)
-    # message_1.write("et")
-    # message_2 = HackerLanguage()
-    #
-    # assert message_1.send() == "111001111001011100011111001011001011110100"
+    message_1.write("secrit")
+    message_1.delete(2)
+    message_1.write("et")
+    message_2 = HackerLanguage()
+
+    assert message_1.send() == "111001111001011100011111001011001011110100"
     # assert message_2.read("11001011101101110000111010011101100") == "email"
