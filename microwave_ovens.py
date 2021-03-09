4c6e1724f3e75613eac47cb8d82a47ee6fc81aac
ogoro <mi.ogoro@gmail.com>
2021-03-10 00:48:12 +0200

    microwave_ovens - time

--- microwave_ovens.py
+++ microwave_ovens.py
@@ -1,5 +1,24 @@
+from datetime import time
+
+
 class MicrowaveBase:
-    pass
+    _time = None
+
+    def __init__(self):
+        self.set_time('00:00')
+
+    def set_time(self, time_string):
+        minutes, seconds = map(int, time_string.split(':'))
+        self._time = time(minute=minutes, second=seconds)
+
+    def add_time(self, amount_of_time_string):
+        if amount_of_time_string.endswith('s'):
+            self._time += time.struct_time(tm_sec=amount_of_time_string[:-1])
+            print('Self time:', self._time)
+        elif amount_of_time_string.endswith('m'):
+            pass
+        else:
+            raise ValueError
 
 
 class Microwave1(MicrowaveBase):
@@ -15,11 +34,19 @@ class Microwave3(MicrowaveBase):
 
 
 class RemoteControl:
-    pass
+    _microwave = None
+
+    def __init__(self, microwave):
+        self._microwave = microwave
+
+    def set_time(self, time_string):
+        self._microwave.set_time(time_string)
+
+    def add_time(self, amount_of_time_string):
+        self._microwave.add_time(amount_of_time_string)
 
 
 if __name__ == '__main__':
-
     microwave_1 = Microwave1()
     microwave_2 = Microwave2()
     microwave_3 = Microwave3()
