92bca63dcbfd2355f6f6b8509c683f1911779959
ogoro <mi.ogoro@gmail.com>
2021-03-11 13:13:45 +0200

    text_editor - show

--- text_editor.py
+++ text_editor.py
@@ -1,13 +1,25 @@
 class Text:
     text = ''
+    font = None
 
     def write(self, new_text):
         self.text += new_text
         print('Self text:', self.text)
 
+    def set_font(self, new_font):
+        self.font = new_font
+        print('Self font:', self.font)
+
+    def show(self):
+        font_tag = self.font and f'[{self.font}]'
+        print('Show text:', font_tag + self.text + font_tag)
+        return font_tag + self.text + font_tag
+
 
 class SavedText:
-    pass
+
+    def save_text(self, text):
+        pass
 
 
 if __name__ == '__main__':
@@ -22,5 +34,5 @@ if __name__ == '__main__':
 
     assert text.show() == "[Arial]At the very beginning there was nothing.[Arial]"
 
-    text.restore(saver.get_version(0))
-    assert text.show() == "At the very beginning "
+    # text.restore(saver.get_version(0))
+    # assert text.show() == "At the very beginning "
