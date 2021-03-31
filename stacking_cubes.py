2250fcc108bb80d5aea0ff22b0f12cd4cdd212e7
ogoro <mi.ogoro@gmail.com>
2021-03-31 21:01:02 +0300

    stacking_cubes - solved

--- stacking_cubes.py
+++ stacking_cubes.py
@@ -5,8 +5,8 @@ def is_touching(a, b):
     #                                           for y in range(a_size)}
     #                                          for ax, ay, a_size in (a[1], b[1])))
 
-    ax, ay, a_size = a[1]
-    bx, by, b_size = b[1]
+    a_id, (ax, ay, a_size) = a
+    b_id, (bx, by, b_size) = b
 
     a_squares = {(ax + x, ay + y) for x in range(a_size) for y in range(a_size)}
     b_squares = {(bx + x, by + y) for x in range(b_size) for y in range(b_size)}
@@ -17,57 +17,28 @@ def is_touching(a, b):
 
 
 def stacking_cubes(cubes):
-    print('Cubes:', cubes)
-    # print()
 
     cubes_with_id = [(cube_id, tuple(cube)) for cube_id, cube in enumerate(cubes)]
-    # print('Cubes with id:', cubes_with_id)
-    # print()
 
     connections = {a: {b for b in cubes_with_id
-                       if a is not b and is_touching(a, b)}
+                       if is_touching(a, b)
+                       and a is not b}
                    for a in cubes_with_id}
 
-    # print('Connections:', connections)
-    # print()
-
-    max_pile_height = max(cube[1][2] for cube in cubes_with_id)
+    max_pile_height = max(cube_size for cube_id, (cube_x, cube_y, cube_size) in cubes_with_id)
     q = [[cube] for cube in cubes_with_id]
-
-    # all_piles = []
 
     while q:
         pile = q.pop()
         a = pile[-1]
 
-        # print('A:', a)
-
         for b in connections[a] - set(pile):
-            # print('    B:', b)
 
             new_pile = pile + [b]
-
-            new_pile_height = sum(cube[1][2] for cube in new_pile)
-
-            if new_pile_height > max_pile_height:
-                # print('    ++++ New max pile height:', new_pile_height)
-                # print('         New pile:           ', new_pile)
-                max_pile_height = new_pile_height
+            new_pile_height = sum(cube_size for cube_id, (cube_x, cube_y, cube_size) in new_pile)
+            max_pile_height = max(max_pile_height, new_pile_height)
 
             q.append(new_pile)
-
-            # all_piles.append(new_pile)
-
-    print('==== Max pile height:', max_pile_height)
-    print()
-
-    # print('Connections:')
-    # [print(key, value) for key, value in connections.items()]
-    # print()
-
-    # print('All piles:')
-    # [print(pile, sum(cube[1][2] for cube in pile)) for pile in all_piles]
-    # print()
 
     return max_pile_height
 
