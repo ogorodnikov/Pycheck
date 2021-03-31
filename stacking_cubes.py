aedb068d30afb603a3bc590fb0bcf1d28ca13868
ogoro <mi.ogoro@gmail.com>
2021-03-31 17:04:19 +0300

    stacking_cubes - connections

---
+++ stacking_cubes.py
@@ -0,0 +1,28 @@
+from typing import List, Tuple
+
+
+def is_touching(a, b):
+    ax, ay, a_size = a
+    bx, by, b_size = b
+    return (ax + a_size > bx >= ax and ay + a_size > by >= ay or
+            bx + b_size > ax >= bx and by + b_size > ay >= by)
+
+
+def stacking_cubes(cubes: List[Tuple[int, int, int]]) -> int:
+    print('Cubes:', cubes)
+    print()
+
+    connections = {a: {b for b in cubes if is_touching(a, b) and a is not b} for a in cubes}
+    print('Connections:', connections)
+    quit()
+
+    return 0
+
+
+if __name__ == '__main__':
+    assert stacking_cubes([(0, 0, 2), (1, 1, 2), (3, 2, 2)]) == 4, 'basic'
+    assert stacking_cubes([(0, 0, 2), (1, 1, 2), (1, 2, 1), (2, 2, 2)]) == 6, 'basic 2'
+    assert stacking_cubes([(0, 0, 2), (2, 0, 2), (2, 0, 2), (0, 2, 2), (0, 2, 2), (0, 2, 2), (0, 2, 2)]) == 8, 'towers'
+    assert stacking_cubes([(0, 0, 2), (0, 3, 2), (3, 0, 2)]) == 2, 'no stacking'
+    assert stacking_cubes([(-1, -1, 2), (0, 0, 2), (-2, -2, 2)]) == 6, 'negative coordinates'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
