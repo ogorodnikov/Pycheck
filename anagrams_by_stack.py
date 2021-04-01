da0e8ccfe016e5edaa2a44b329c945de740d0a79
ogoro <mi.ogoro@gmail.com>
2021-04-01 03:13:24 +0300

    anagrams_by_stack - lengths

--- anagrams_by_stack.py
+++ anagrams_by_stack.py
@@ -1,5 +1,128 @@
 def checkio(data):
-    return ""
+
+    start, end = data.split('-')
+
+    start_parts = (start, '', '')
+
+    min_actions_len = float('inf')
+    history = {start_parts}
+    q = [(start_parts, [])]
+
+    while q:
+        (a, b, c), actions = q.pop()
+
+        if a:
+            a1 = a[:-1]
+            b1 = b + a[-1]
+            c1 = c
+
+            action = '10'
+
+            new_parts = (a1, b1, c1)
+
+            if new_parts[2] == end:
+                new_actions = actions + [action]
+                new_actions_len = len(actions + [action])
+                print('New parts:', new_parts)
+                print('Actions:', new_actions, new_actions_len)
+
+                if new_actions_len < min_actions_len:
+                    print('++++ New min length:', new_actions_len)
+                    min_actions_len = new_actions_len
+
+
+                quit()
+            if new_parts not in history:
+                q.append((new_parts, actions + [action]))
+                history.add(new_parts)
+
+            a2 = a[:-1]
+            b2 = b
+            c2 = c + a[-1]
+
+            action = '12'
+
+            new_parts = (a2, b2, c2)
+
+            if new_parts[2] == end:
+                print('New parts:', new_parts)
+                print('Actions:', actions + [action], len(actions + [action]))
+                quit()
+            if new_parts not in history:
+                q.append((new_parts, actions + [action]))
+                history.add(new_parts)
+
+        if b:
+            b1 = b[:-1]
+            a1 = a + b[-1]
+            c1 = c
+
+            action = '01'
+
+            new_parts = (a1, b1, c1)
+
+            if new_parts[2] == end:
+                print('New parts:', new_parts)
+                print('Actions:', actions + [action], len(actions + [action]))
+                quit()
+            if new_parts not in history:
+                q.append((new_parts, actions + [action]))
+                history.add(new_parts)
+
+            b2 = b[:-1]
+            a2 = a
+            c2 = c + b[-1]
+
+            action = '02'
+
+            new_parts = (a2, b2, c2)
+
+            if new_parts[2] == end:
+                print('New parts:', new_parts)
+                print('Actions:', actions + [action], len(actions + [action]))
+                quit()
+            if new_parts not in history:
+                q.append((new_parts, actions + [action]))
+                history.add(new_parts)
+
+        if c:
+            c1 = c[:-1]
+            a1 = a + c[-1]
+            b1 = b
+
+            action = '21'
+
+            new_parts = (a1, b1, c1)
+
+            if new_parts[2] == end:
+                print('New parts:', new_parts)
+                print('Actions:', actions + [action], len(actions + [action]))
+                quit()
+            if new_parts not in history:
+                q.append((new_parts, actions + [action]))
+                history.add(new_parts)
+
+            c2 = c[:-1]
+            a2 = a
+            b2 = b + c[-1]
+
+            action = '20'
+
+            new_parts = (a2, b2, c2)
+
+            if new_parts[2] == end:
+                print('New parts:', new_parts)
+                print('Actions:', actions + [action], len(actions + [action]))
+                quit()
+            if new_parts not in history:
+                q.append((new_parts, actions + [action]))
+                history.add(new_parts)
+
+        print('Q:', q)
+        print('History:', history)
+        print()
+
+    return ValueError
 
 
 if __name__ == '__main__':
