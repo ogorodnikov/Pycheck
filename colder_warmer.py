9a10e252c1cee308511fdf79b13131b25d0ccf5f
ogoro <mi.ogoro@gmail.com>
2021-04-07 00:55:18 +0300

    colder_warmer - solved - optimised

--- colder_warmer.py
+++ colder_warmer.py
@@ -1,77 +1,61 @@
 BOARD_SIZE = 10
 
-board = {complex(y, x) for x in range(BOARD_SIZE) for y in range(BOARD_SIZE)}
-possible_cells = board.copy()
-last_step_cell = None
+possible_cells = {complex(y, x) for x in range(BOARD_SIZE) for y in range(BOARD_SIZE)}
+previous_cell = None
 
-def print_board(cells):
-
-    height = int(max(cell.real for cell in cells)) + 1
-    width = int(max(cell.imag for cell in cells)) + 1
-
-    print(''.join(map(str, range(width))))
-    for y in range(height):
-        row = ''
-        for x in range(width):
-            cell = complex(y, x)
-            if cell in cells:
-                row += 'X'
-            else:
-                row += '.'
-        print(row)
 
 def closer_cells(closer_cell, further_cell):
-    return (cell for cell in possible_cells if abs(cell - closer_cells()) < abs(cell - further_cell))
+    return {cell for cell in possible_cells
+            if abs(cell - closer_cell) < abs(cell - further_cell)}
+
 
 def checkio(steps):
+    global possible_cells
+    global previous_cell
 
     y, x, result = steps[-1]
-    current_step = complex(y, x)
+    current_cell = complex(y, x)
 
-
-    global possible_cells
-    global last_step_cell
-
+    possible_cells -= {current_cell}
 
     if result == 1:
-
-        closer_cells = {cell for cell in possible_cells if abs(cell - current_step) < abs(cell - last_step_cell)}
-        possible_cells &= closer_cells
+        possible_cells &= closer_cells(current_cell, previous_cell)
 
     elif result == -1:
-
-        further_cells = {cell for cell in possible_cells if abs(cell - current_step) > abs(cell - last_step_cell)}
-        possible_cells &= further_cells
-
-    last_step_cell = current_step
-
-    # print('New Last step cell:', last_step_cell)
-
-    # random_cell = random.choice(list(possible_cells))
-    # print('Random cell:', random_cell)
-    # new_step_cell = random_cell
-
-    possible_cells -= {current_step}
-
-    # print('    Possible cells:', possible_cells)
-    # print_board(possible_cells)
-
-    # random_cell = random.choice(list(possible_cells))
-    # next_step = random_cell
-
-    # print('Next step:', next_step)
+        possible_cells &= closer_cells(previous_cell, current_cell)
 
     next_step = min(possible_cells,
                     key=lambda new_cell: abs(
-                        sum(abs(cell - new_cell) < abs(cell - current_step) for cell in possible_cells) -
-                        sum(abs(cell - new_cell) > abs(cell - current_step) for cell in possible_cells)))
+                        len(closer_cells(new_cell, current_cell)) -
+                        len(closer_cells(current_cell, new_cell))))
 
-    return list(map(int, (next_step.real, next_step.imag)))
+    previous_cell = current_cell
+
+    print_possible_cells()
+    print('Next step:', next_step)
+
+    next_step_coordinates = list(map(int, (next_step.real, next_step.imag)))
+    return next_step_coordinates
+
+
+def print_possible_cells():
+    height = int(max(cell.real for cell in possible_cells)) + 1
+    width = int(max(cell.imag for cell in possible_cells)) + 1
+
+    print(''.join(map(str, range(width))))
+    for y in range(height):
+        row = ''.join('X' if complex(y, x) in possible_cells else '.'
+                      for x in range(width))
+        print(row)
+
+
 
 if __name__ == '__main__':
 
     from math import hypot
+
     MAX_STEP = 12
+
 
     def check_solution(func, goal, start):
         prev_steps = [start]
@@ -88,6 +72,7 @@ if __name__ == '__main__':
             prev_steps.append([row, col, alteration])
         print("Too many steps")
         return False
+
 
     assert check_solution(checkio, [7, 7], [5, 5, 0]), "1st example"
     # assert check_solution(checkio, [5, 6], [0, 0, 0]), "2nd example"
