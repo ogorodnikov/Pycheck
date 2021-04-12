69a12ca542db3a9d4d6dd70c6e00f1d83f6fb7d0
ogoro <mi.ogoro@gmail.com>
2021-04-12 23:10:17 +0300

    simplification - one coefficient star fix

--- simplification.py
+++ simplification.py
@@ -229,13 +229,13 @@ def polynomial_to_string(polynomial):
         else:
             degree_string = f'x**{degree}'
 
-        # print('Coefficient:', coefficient)
-        # print('Degree:', degree)
-        # print('    Coefficient string:', coefficient_string)
-        # print('    Degree string:', degree_string)
-        # print()
+        print('Coefficient:', coefficient)
+        print('Degree:', degree)
+        print('    Coefficient string:', coefficient_string)
+        print('    Degree string:', degree_string)
+        print()
 
-        if coefficient_string == '-':
+        if coefficient_string in '-+':
             term_symbol = ''
         else:
             term_symbol = '*'
@@ -272,10 +272,11 @@ if __name__ == "__main__":
     assert simplify("x*x*x+5*x*x+x*x+3*x-1") == "x**3+6*x**2+3*x-1"
 
     assert simplify("(x-1)*(x+1)") == "x**2-1", "First and simple"
+    assert simplify("(x+1)*(x+1)") == "x**2+2*x+1", "Almost the same"
+    assert simplify("(x+3)*x*2-x*x") == "x**2+6*x", "Different operations"
 
-    # assert simplify("(x+1)*(x+1)") == "x**2+2*x+1", "Almost the same"
-    # assert simplify("(x+3)*x*2-x*x") == "x**2+6*x", "Different operations"
-    # assert simplify("x+x*x+x*x*x") == "x**3+x**2+x", "Don't forget about order"
+    assert simplify("x+x*x+x*x*x") == "x**3+x**2+x", "Don't forget about order"
+
     # assert simplify("(2*x+3)*2-x+x*x*x*x") == "x**4+3*x+6", "All together"
     # assert simplify("x*x-(x-1)*(x+1)-1") == "0", "Zero"
     # assert simplify("5-5-x") == "-x", "Negative C1"
