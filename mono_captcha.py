fde4d0d4c096e97b8cce3851bc20ca5f808f39af
ogoro <mi.ogoro@gmail.com>
2021-04-13 18:08:20 +0300

    mono_captcha - solved

--- mono_captcha.py
+++ mono_captcha.py
@@ -2,9 +2,9 @@ from itertools import starmap
 from operator import sub
 from typing import List
 
+MARK = 'X'
 SPACE_WIDTH = 1
 SYMBOL_WIDTH = 3
-MARK = 'X'
 
 FONT = ("--X--XXX-XXX-X-X-XXX--XX-XXX-XXX--XX-XX--",
         "-XX----X---X-X-X-X---X-----X-X-X-X-X-X-X-",
@@ -14,30 +14,43 @@ FONT = ("--X--XXX-XXX-X-X-XXX--XX-XXX-XXX--XX-XX--",
 
 
 def parse_image(image):
+
     return list(zip(*([row[i + SPACE_WIDTH:i + SPACE_WIDTH + SYMBOL_WIDTH]
                        for i in range(0, len(row) - SPACE_WIDTH, SYMBOL_WIDTH + SPACE_WIDTH)]
                       for row in image)))
 
 
 def strings_to_binary(strings):
+
     return [[[1 if cell == MARK else 0 for cell in list(line)]
              for line in number]
             for number in strings]
 
 
+def find_digits(symbol_image, template_image):
+
+    return [min((sum(sum(map(abs, starmap(sub, zip(*line_pair))))
+                     for line_pair in zip(symbol, template)), digit % 10)
+                for digit, template in enumerate(template_image, 1))[1]
+            for symbol in symbol_image]
+
+
+def digits_to_number(digits):
+
+    return sum(digit * 10 ** position for position, digit in (enumerate(reversed(digits))))
+
+
 def checkio(image: List[List[int]]) -> int:
+
     template_strings = parse_image(FONT)
 
-    template_images = strings_to_binary(template_strings)
+    template_image = strings_to_binary(template_strings)
 
-    symbol_images = parse_image(image)
+    symbol_image = parse_image(image)
 
-    digits = [min((sum(sum(map(abs, starmap(sub, zip(*line_pair))))
-                       for line_pair in zip(symbol, template)), digit)
-                  for digit, template in enumerate(template_images, 1))[1]
-              for symbol in symbol_images]
+    digits = find_digits(symbol_image, template_image)
 
-    number = sum(digit * 10 ** position for position, digit in (enumerate(reversed(digits))))
+    number = digits_to_number(digits)
 
     print('Image:')
     [print(''.join(MARK if cell == 1 else ' ' for cell in line)) for line in image]
@@ -47,6 +60,7 @@ def checkio(image: List[List[int]]) -> int:
     # print('Template images:', template_images)
     # print('Symbol images:', symbol_images)
     # print('Digits:', digits)
+
     print('Number:', number)
     print()
     return number
