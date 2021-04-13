a96e2d6dba2f3aafac98d2e1ebd56d505ace5100
ogoro <mi.ogoro@gmail.com>
2021-04-13 20:57:46 +0300

    fractions_addition

---
+++ fractions_addition.py
@@ -0,0 +1,9 @@
+def add_fractions(fracts):
+    #replace this for solution
+    return 0
+
+if __name__ == '__main__':
+
+    assert add_fractions(((2, 3), (2, 3))) == "1 and 1/3"
+    assert add_fractions(((1, 3), (1, 3))) == "2/3"
+    assert add_fractions(((1, 3), (1, 3), (1, 3))) == 1
